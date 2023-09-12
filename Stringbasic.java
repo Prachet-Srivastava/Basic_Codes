d6446051db84c3f7c8168bfe940d22ff3df30927
Prachet Nandan <131990865+Prachet-Srivastava@users.noreply.github.com>
2023-09-12 22:45:14 +0530

    Add files via upload

---
+++ Stringbasic.java
@@ -0,0 +1,17 @@
+public class Stringbasic {
+    public static void main(String[] args) {
+        // String name= "prachet";
+        // String name1= new String("Srivastava");
+        // System.out.println(name.equals(name1));
+        // System.out.println(name.charAt(0));
+
+        float a= 534.4482f;
+      //  System.out.printf("Formatted String is %.2f",a);    
+
+       // System.out.printf("Pie Value %.3f",Math.PI);
+
+       
+        System.out.printf("Hello I am %s and I am %s","Prachet","Coder");
+    }
+
+}
